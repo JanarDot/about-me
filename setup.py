1ada5897296b7b13c23c9a8eac5af8a2eb2865b7
Jana <mailjanardot@gmail.com>
2026-04-15 10:12:57 +0100

    Include README in package metadata

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+from pathlib import Path
+
 from setuptools import setup
+
+
+long_description = Path("README.md").read_text(encoding="utf-8")
 
 
 setup(
     name="janarun",
     version="0.1.0",
     description="A terminal utility.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/JanarDot/about-me",
     author="Jana",
     author_email="hello@clandestinalabs.com",
